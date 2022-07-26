7f5ee62e4fb37ce15acc35011b12ba937d7d4896
HilalVildan <hilalsarioglu96@gmail.com>
2022-07-26 13:07:23 +0200

    python sound and teamwork document added

---
+++ hilmi_icin.py
@@ -0,0 +1,2 @@
+from text_to_speech import speak
+speak("Evde hic su kalmamis gelirken su alir misin askim benim", "tr", save=True, file="hilmi_icin.mp3", speak=True)
---
+++ seslendirme.py
@@ -0,0 +1,3 @@
+
+from text_to_speech import speak
+speak("Merhaba arkadaşlar Python dersi nasıl gidiyor", "tr", save=True, file="merhaba.mp3", speak=True)
